dc6b8d9612d701a6d5ec022e6804d5e5fa4c9926
Artur.Mag <coungard@mail.ru>
2022-11-02 12:28:36 +0300

    add gateway

--- gateway/src/main/java/com/coungard/GatewayApplication.java
+++ gateway/src/main/java/com/coungard/GatewayApplication.java
@@ -2,13 +2,13 @@ package com.coungard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServerApplication {
+@EnableEurekaClient
+public class GatewayApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(EurekaServerApplication.class, args);
+    SpringApplication.run(GatewayApplication.class, args);
   }
 }
