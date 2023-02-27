53f02caf95c21658c7ba45a60c5c939764587b5d
Artur.Mag <coungard@mail.ru>
2023-02-27 23:55:12 +0300

    Entities for order service

--- order-service/src/main/java/com/coungard/entity/Parcel.java
+++ order-service/src/main/java/com/coungard/entity/Parcel.java
@@ -1,14 +1,11 @@
 package com.coungard.entity;
 
-import com.coungard.model.DeliveryOrderStatus;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,25 +13,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "delivery_order")
+@Table(name = "parcel")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class DeliveryOrder {
+@Builder
+public class Parcel {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private DeliveryOrderStatus deliveryOrderStatus;
-  private String city;
-  private String district;
-  private String receiver_name;
-  private String receiver_surname;
-  private String receiver_phone;
-  @OneToMany
-  @Builder.Default
-  private Set<Parcel> parcels = new HashSet<>();
+  private String type;
+  private Double weight;
 
+  @ManyToOne
+  private DeliveryOrder deliveryOrder;
   private String createdBy;
   private LocalDateTime createdDate;
   private String lastModifiedBy;
