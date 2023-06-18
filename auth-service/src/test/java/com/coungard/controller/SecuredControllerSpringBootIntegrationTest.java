0a3de2971f2cbeb99d150e98d668b0fbfaab0e2e
Artur.Mag <coungard@mail.ru>
2023-06-18 17:11:53 +0300

    init integration test

--- auth-service/src/test/java/com/coungard/controller/SecuredControllerSpringBootIntegrationTest.java
+++ auth-service/src/test/java/com/coungard/controller/SecuredControllerSpringBootIntegrationTest.java
@@ -1,13 +1,20 @@
 package com.coungard.controller;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.coungard.model.request.LoginRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -16,12 +23,13 @@ import org.springframework.web.context.WebApplicationContext;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
 public class SecuredControllerSpringBootIntegrationTest {
 
   @Autowired
   private WebApplicationContext context;
 
-  private MockMvc mvc; // todo integration tests
+  private MockMvc mvc;
 
   @BeforeAll
   public void setup() {
@@ -29,5 +37,26 @@ public class SecuredControllerSpringBootIntegrationTest {
         .webAppContextSetup(context)
         .apply(springSecurity())
         .build();
+  }
+
+  @Test
+  public void loginSuccess() throws Exception {
+    LoginRequest loginRequest = LoginRequest.builder()
+        .email("user@gmail.com")
+        .password("12345678")
+        .build();
+
+    mvc.perform(post("/auth/login")
+            .content(asJsonString(loginRequest))
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+  }
+
+  private static String asJsonString(final Object obj) {
+    try {
+      return new ObjectMapper().writeValueAsString(obj);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 }
